8a848ae8635eff810e8b5769257e34e55fb2eee7
Elvis Shi <shonetesmith@gmail.com>
2018-03-09 02:21:59 +0900

    Create zundoko.rb

---
+++ Casks/zundoko.rb
@@ -0,0 +1,10 @@
+cask 'zundoko' do
+  version '0.1'
+  sha256 '2b4ba51564bdef21ab86baf093c214712345e68988de364f73b23facd8cb9f2a'
+
+  url 'https://github.com/el-hoshino/zundoko/releases/download/0.1/zundoko.zip'
+  name 'zundoko'
+  homepage 'https://github.com/el-hoshino/zundoko'
+
+  binary 'zundoko'
+end
